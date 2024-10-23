fd130d52bf83a87958d5b77cdc06adc701182272
Connor Dailey <connorldailey@gmail.com>
2024-10-22 21:00:00 -0400

    Fixed bugs in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,15 +4,18 @@ CREATE DATABASE movie_db;
 \c movie_db
 
 DROP TABLE IF EXISTS movies;
-CREATE TABLE movie_names (
+CREATE TABLE movies (
     id SERIAL PRIMARY KEY,
     movie_name VARCHAR(100)
 );
 
 DROP TABLE IF EXISTS reviews;
-CREATE TABLE movie_reviews (
+CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
     movie_id INT,
-    review TEXT
+    review TEXT,
+    FOREIGN KEY (movie_id)
+    REFERENCES movies(id)
+    ON DELETE SET NULL
 );
 
