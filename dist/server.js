3fd000655efce63d711343a026c8cd2f4afb5298
Connor Dailey <connorldailey@gmail.com>
2024-10-22 21:19:08 -0400

    Add server and connection

--- dist/server.js
+++ dist/server.js
@@ -1,33 +1,24 @@
 import express from 'express';
-import { QueryResult } from 'pg';
 import { pool, connectToDb } from './connection.js';
-
 await connectToDb();
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 // GET a list of all movies
-pool.query(
-    'SELECT * FROM movies',
-    (err: Error, result: QueryResult) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(result.rows);
-        }
+pool.query('SELECT * FROM movies', (err, result) => {
+    if (err) {
+        console.log(err);
     }
-)
-
+    else {
+        console.log(result.rows);
+    }
+});
 // Default response for any other request
-app.use((_req,  res) => {
+app.use((_req, res) => {
     res.status(404).end();
 });
-
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
